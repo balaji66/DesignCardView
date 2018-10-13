4e784369b84810061d202fc6935182d66bbf8f8b
Balaji66 <pusuluribalaji66@gmail.com>
2018-10-13 23:13:39 +0530

    final commit

--- app/src/main/java/com/durga/balaji66/designcardview/MainActivity.java
+++ app/src/main/java/com/durga/balaji66/designcardview/MainActivity.java
@@ -30,12 +30,12 @@ public class MainActivity extends AppCompatActivity {
     }
     public void addItemsToList()
     {
-        list.add(0, new ModelClass("Google.com","Balaji","1231231231","balaji@gmail.com"));
-        list.add(1, new ModelClass("Google.com","Rohit","1111111111","rohit@gmail.com"));
-        list.add(2, new ModelClass("Google.com","Kiran","2222222222","kiran@gmail.com"));
-        list.add(3, new ModelClass("Google.com","Prashanth","3333333333","prashanth@gmail.com"));
-        list.add(4, new ModelClass("Google.com","Amit","4444444444","amit@gmail.com"));
-        list.add(5, new ModelClass("Google.com","Roshan","5555555555","roshan@gmail.com"));
+        list.add(0, new ModelClass("Google","Balaji","1231231231","balaji@gmail.com"));
+        list.add(1, new ModelClass("Facebook","Rohit","1111111111","rohit@gmail.com"));
+        list.add(2, new ModelClass("Instagram","Kiran","2222222222","kiran@gmail.com"));
+        list.add(3, new ModelClass("TCS","Prashanth","3333333333","prashanth@gmail.com"));
+        list.add(4, new ModelClass("Infosys","Amit","4444444444","amit@gmail.com"));
+        list.add(5, new ModelClass("TCS","Roshan","5555555555","roshan@gmail.com"));
 
 
 
